32f817c32d0432e4089fb2fe0001704d550389b7
JaiRaga <raga.j.santhosh@gmail.com>
2020-07-21 20:49:44 +0530

    created blog component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Blogs from "./component/blogs/Blogs";
+import "./App.css";
+import Blog from "./component/blogs/Blog";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Blog />
+      {/* <Blogs /> */}
     </div>
   );
 }
