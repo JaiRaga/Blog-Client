b2a96d1b51cc37e63317fafb53524e11360fa8cb
JaiRaga <raga.j.santhosh@gmail.com>
2020-08-23 16:04:02 +0530

    displays individual trending topics

---
+++ src/component/trending/TrendingBlogItem.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+
+const TrendingBlogItem = () => {
+  return (
+    <Grid item>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis
+      repellat illum officia quo sit omnis nostrum provident. Saepe facere, quia
+      accusantium amet fuga minima. Pariatur ullam laborum maxime adipisci
+      blanditiis iure quam, veniam cumque illum nobis obcaecati maiores,
+      doloremque quis?
+    </Grid>
+  );
+};
+
+export default TrendingBlogItem;
