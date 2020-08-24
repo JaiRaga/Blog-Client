a0801219f0981fcd0b5fc7359b82dc4c47c9f8dd
JaiRaga <raga.j.santhosh@gmail.com>
2020-08-24 15:24:15 +0530

    refactored component to bereused in trending component

--- src/component/blogs/BlogItem.js
+++ src/component/blogs/BlogItem.js
@@ -1,31 +1,48 @@
 import React from "react";
-import { Grid, Typography, Button } from "@material-ui/core";
+import { Grid, Typography, Button, makeStyles } from "@material-ui/core";
 
-const BlogItem = () => {
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     flexGrow: 1
+//   },
+//   tags: {
+//     [theme.breakpoints.down("xs")]: {}
+//   }
+// }));
+
+const BlogItem = ({ trending }) => {
+  // const classes = useStyles();
+  const mediumScreenTitle = trending ? 12 : 4;
+  const mediumScreenTags = trending ? 12 : 8;
+
   return (
-    <Grid container item>
-      <Grid item xs={12} md={4}>
-        <Typography variant='h4'>Blog Title </Typography>
+    <Grid container item justify='center'>
+      <Grid item xs={12} md={mediumScreenTitle}>
+        <Typography variant='h4' align='center'>
+          Blog Title
+        </Typography>
       </Grid>
-      <Grid item xs={12} md={5}>
-        <Button variant='contained' color='primary'>
-          Tag1
-        </Button>
-        <Button variant='contained' color='primary'>
-          Tag2
-        </Button>
-        <Button variant='contained' color='primary'>
-          Tag3
-        </Button>
-        <Button variant='contained' color='primary'>
-          Tag4
-        </Button>
-        <Button variant='contained' color='primary'>
-          Tag5
-        </Button>
-        <Button variant='contained' color='primary'>
-          Tag6
-        </Button>
+      <Grid item xs={12} md={mediumScreenTags}>
+        <Grid container item justify='center'>
+          <Button variant='contained' color='primary'>
+            Tag1
+          </Button>
+          <Button variant='contained' color='primary'>
+            Tag2
+          </Button>
+          <Button variant='contained' color='primary'>
+            Tag3
+          </Button>
+          <Button variant='contained' color='primary'>
+            Tag4
+          </Button>
+          <Button variant='contained' color='primary'>
+            Tag5
+          </Button>
+          <Button variant='contained' color='primary'>
+            Tag6
+          </Button>
+        </Grid>
       </Grid>
     </Grid>
   );
