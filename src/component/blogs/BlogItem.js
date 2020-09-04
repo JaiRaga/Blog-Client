b6ee8f38106406cb7d4bd7fed32098f63094492d
JaiRaga <raga.j.santhosh@gmail.com>
2020-09-04 15:43:31 +0530

    passed props from parent blog container

--- src/component/blogs/BlogItem.js
+++ src/component/blogs/BlogItem.js
@@ -1,50 +1,43 @@
-import React from "react";
-import { Grid, Typography, Button, makeStyles } from "@material-ui/core";
+import React, { Fragment } from "react";
+import {
+  Grid,
+  Typography,
+  Button,
+  makeStyles,
+  Divider
+} from "@material-ui/core";
 
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     flexGrow: 1
-//   },
-//   tags: {
-//     [theme.breakpoints.down("xs")]: {}
-//   }
-// }));
+const useStyles = makeStyles((theme) => ({
+  divider: {
+    margin: 5
+  }
+}));
 
-const BlogItem = ({ trending }) => {
-  // const classes = useStyles();
+const BlogItem = ({ blog, trending }) => {
+  const classes = useStyles();
   const mediumScreenTitle = trending ? 12 : 4;
   const mediumScreenTags = trending ? 12 : 8;
 
   return (
-    <Grid container item justify='center'>
-      <Grid item xs={12} md={mediumScreenTitle}>
-        <Typography variant='h4' align='center'>
-          Blog Title
-        </Typography>
-      </Grid>
-      <Grid item xs={12} md={mediumScreenTags}>
-        <Grid container item justify='center'>
-          <Button variant='contained' color='primary'>
-            Tag1
-          </Button>
-          <Button variant='contained' color='primary'>
-            Tag2
-          </Button>
-          <Button variant='contained' color='primary'>
-            Tag3
-          </Button>
-          <Button variant='contained' color='primary'>
-            Tag4
-          </Button>
-          <Button variant='contained' color='primary'>
-            Tag5
-          </Button>
-          <Button variant='contained' color='primary'>
-            Tag6
-          </Button>
+    <Fragment>
+      <Grid container item justify='center'>
+        <Grid item xs={12} md={mediumScreenTitle}>
+          <Typography variant='h4' align='center'>
+            {blog.title}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} md={mediumScreenTags}>
+          <Grid container item justify='center'>
+            {blog.category.map((tag) => (
+              <Button variant='contained' color='primary'>
+                {tag}
+              </Button>
+            ))}
+          </Grid>
         </Grid>
       </Grid>
-    </Grid>
+      <Divider className={classes.divider} />
+    </Fragment>
   );
 };
 
