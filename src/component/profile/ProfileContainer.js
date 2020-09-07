9742c81b6561fab159ae2d186552fc561d85e0a6
JaiRaga <raga.j.santhosh@gmail.com>
2020-09-07 16:07:02 +0530

    component to display user profiles

---
+++ src/component/profile/ProfileContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+import Profile from "./Profile";
+
+const ProfileContainer = () => {
+  return (
+    <Grid container>
+      <Profile />
+    </Grid>
+  );
+};
+
+export default ProfileContainer;
