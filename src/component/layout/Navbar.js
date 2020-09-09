df597b7b141fc449faf1794d7cf36d76c9805c67
JaiRaga <raga.j.santhosh@gmail.com>
2020-09-09 15:55:54 +0530

    added search option

--- src/component/layout/Navbar.js
+++ src/component/layout/Navbar.js
@@ -1,8 +1,10 @@
 import React, { Fragment } from "react";
 import {
+  fade,
   makeStyles,
   AppBar,
   Toolbar,
+  InputBase,
   IconButton,
   Typography,
   Button,
@@ -19,11 +21,54 @@ import HomeIcon from "@material-ui/icons/Home";
 import PersonPinIcon from "@material-ui/icons/PersonPin";
 import DirectionsRunIcon from "@material-ui/icons/DirectionsRun";
 import AdjustIcon from "@material-ui/icons/Adjust";
+import SearchIcon from "@material-ui/icons/Search";
 import { Link, Redirect } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 // import { logout } from "../../Redux/actions/auth";
 
 const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25)
+    },
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(1),
+      width: "auto"
+    }
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  inputRoot: {
+    color: "inherit"
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch"
+      }
+    }
+  },
   navbar: {
     [theme.breakpoints.down("md")]: {
       display: "none"
@@ -123,6 +168,20 @@ const Navbar = () => {
         </Link>
       </List>
       <List className={classes.right}>
+        <div className={classes.search}>
+          <div className={classes.searchIcon}>
+            <SearchIcon />
+          </div>
+          <InputBase
+            placeholder='Search…'
+            classes={{
+              root: classes.inputRoot,
+              input: classes.inputInput
+            }}
+            inputProps={{ "aria-label": "search" }}
+          />
+        </div>
+
         <Link to='/login' className={classes.link}>
           <ListItem button>
             <ListItemIcon className={classes.icons}>
@@ -163,9 +222,13 @@ const Navbar = () => {
   );
 
   return (
-    <AppBar position='static' className={classes.navbar}>
-      <Toolbar>{isAuthenticated && !loading ? authLinks : guestLinks}</Toolbar>
-    </AppBar>
+    <div className={classes.root}>
+      <AppBar position='static' className={classes.navbar}>
+        <Toolbar>
+          {isAuthenticated && !loading ? authLinks : guestLinks}
+        </Toolbar>
+      </AppBar>
+    </div>
   );
 };
 
