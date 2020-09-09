222b09bd91b684d13df30c459f7449fcd06c7ed0
JaiRaga <raga.j.santhosh@gmail.com>
2020-09-09 13:19:16 +0530

    modified layout

--- src/component/profile/Profile.js
+++ src/component/profile/Profile.js
@@ -1,10 +1,16 @@
 import React from "react";
-import { Grid } from "@material-ui/core";
+import { Grid, Avatar } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 
 const useStyles = makeStyles((theme) => ({
-  grid: {
-    backgroundColor: "#eee"
+  // grid: {
+  //   backgroundColor: "#eee"
+  // }
+  avatar: {
+    marginRight: 10
+  },
+  gridRight: {
+    marginRight: "auto"
   }
 }));
 
@@ -17,22 +23,28 @@ const Profile = () => {
       direction='column'
       justify='center'
       alignItems='center'
-      className={classes.grid}
-      xs={3}
-      md={4}>
-      <Grid container item justify='space-evenly' alignItems='center'>
-        <Grid item>Pic</Grid>
-        <Grid item>
+      xs={12}
+      md={4}
+      className={classes.grid}>
+      <Grid container item justify='space-between' alignItems='center'>
+        <Avatar
+          className={classes.avatar}
+          alt='pic'
+          src='https://images.pexels.com/photos/2280845/pexels-photo-2280845.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'
+        />
+        <Grid item className={classes.gridRight}>
           <Grid container item direction='column'>
             <Grid item>Name</Grid>
             <Grid item>rating</Grid>
           </Grid>
         </Grid>
       </Grid>
-      <grid item>Blogs</grid>
-      <grid item>Trending</grid>
-      <grid item>Suggestions</grid>
-      <grid item>Tags</grid>
+      <Grid container item direction='column'>
+        <Grid item>Blogs</Grid>
+        <Grid item>Trending</Grid>
+        <Grid item>Suggestions</Grid>
+        <Grid item>Tags</Grid>
+      </Grid>
     </Grid>
   );
 };
