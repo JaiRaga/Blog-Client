a8f32d0229c4035bda0a9f9403b33a98ebd6ce16
JaiRaga <raga.j.santhosh@gmail.com>
2020-09-09 13:18:43 +0530

    added ProfileDisplay component

--- src/component/profile/ProfileContainer.js
+++ src/component/profile/ProfileContainer.js
@@ -1,11 +1,27 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
 import Profile from "./Profile";
+import ProfileDisplay from "./ProfileDisplay";
+
+const useStyles = makeStyles((theme) => ({
+  grid: {
+    backgroundColor: "#999",
+    margin: 5,
+    padding: 20
+  }
+}));
 
 const ProfileContainer = () => {
+  const classes = useStyles();
   return (
-    <Grid container>
-      <Profile />
+    <Grid container justify='center'>
+      <Grid item xs={5} md={3} className={classes.grid}>
+        <Profile />
+      </Grid>
+      <Grid item className={classes.grid} xs={12} md={6}>
+        <ProfileDisplay />
+      </Grid>
     </Grid>
   );
 };
