1f9cc380f0e3b9f670bba67c7eb3caf6fd0e6665
JaiRaga <raga.j.santhosh@gmail.com>
2020-09-20 15:32:22 +0530

    connected with redux store

--- src/component/dashboard/Dashboard.js
+++ src/component/dashboard/Dashboard.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Grid, makeStyles } from "@material-ui/core";
+import { useDispatch } from "react-redux";
 import CreateBlogBtn from "../blogs/CreateBlogBtn";
 import Blogs from "../blogs/Blogs";
 import BlogItem from "../blogs/BlogItem";
 import Search from "../search/Search";
+import { getBlogs } from "../../redux/actions/blog";
 
 const useStyles = makeStyles((theme) => ({
   btn: {
@@ -13,6 +15,11 @@ const useStyles = makeStyles((theme) => ({
 
 const Dashboard = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getBlogs());
+  }, []);
   return (
     <Grid container direction='column' alignItems='center'>
       <Grid item xs={6} className={classes.btn}>
